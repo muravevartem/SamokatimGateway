e5c9baf6544a92b447d26af3b67f36c5b863e847
muravevas <muravevartem@outlook.com>
2023-01-29 11:58:39 +0400

    Initial commit

---
+++ src/test/java/com/muravev/samokatimgateway/SamokatimGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muravev.samokatimgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamokatimGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
